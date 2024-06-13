e36cb45137edfc5b4d3ccafdc59f91d0ced962bb
EgitaZ <egita.s@inbox.lv>
2024-06-13 23:21:28 +0300

    Nodarbba_02a

--- Nodarbiba_03/JS/first_script.js/second_script.js
+++ Nodarbiba_03/JS/first_script.js/second_script.js
@@ -1,1 +1,17 @@
 a = 10;
+console.log("a mainīgā vērtība:" + a);
+
+var x = 10;
+console.log("x mainīgā vērtība pirms{}:" + x);
+{
+    console.log("x mainīgā vērtība iekš {}:" + x);
+    x = 20;
+    console.log("x mainīgā vērtība iekš {} un pēc = darbības:" + x);
+    var x = 100;
+    console.log("x mainīgā vērtība iekš {} un pēc atkārtotas var:" + x);
+}
+console.log("x mainīgā vērtība pēc {}: " + x);
+x = 30;
+console.log("x mainīgā vērtība pēc {} un pēc = darbības:" + x);
+var x = 40;
+console.log("x mainīgā vērtība pēc {} un pēc atkārtotas var:" + x);
