03030c18017786327f7997fb8aae645a67151ebf
EgitaZ <egita.s@inbox.lv>
2024-06-13 23:50:27 +0300

    Nodarbiba_02a

--- Nodarbiba_03/JS/first_script.js/second_script.js
+++ Nodarbiba_03/JS/first_script.js/second_script.js
@@ -1,6 +1,6 @@
 a = 10;
 console.log("a mainīgā vērtība:" + a);
-
+/*--------------------------------tests ar var------------------------------*/
 var x = 10;
 console.log("x mainīgā vērtība pirms{}:" + x);
 {
@@ -15,3 +15,20 @@ x = 30;
 console.log("x mainīgā vērtība pēc {} un pēc = darbības:" + x);
 var x = 40;
 console.log("x mainīgā vērtība pēc {} un pēc atkārtotas var:" + x);
+/*--------------------------------tests ar let------------------------------*/
+let y = 10; // summas mainiigais
+console.log("y mainīgā vērtība pirms{}:" + y);
+{
+    console.log("y mainīgā vērtība iekš {}:" + y);
+    x = 20;
+    console.log("y mainīgā vērtība iekš {} un pēc = darbības:" + y);
+    let y = 100;
+    console.log("y mainīgā vērtība iekš {} un pēc atkārtotas let:" + y);
+}
+console.log("y mainīgā vērtība pēc {}: " + y);
+y = 30;
+console.log("y mainīgā vērtība pēc {} un pēc = darbības:" + y);
+// let y = 40;
+// var y = 40;
+// const y = 40; 
+console.log("y mainīgā vērtība pēc {} un pēc atkārtotas let:" + y);
