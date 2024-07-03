a18c13b1ec54d83763d853633ef9cc34022775b2
EgitaZ <egita.s@inbox.lv>
2024-07-03 11:40:11 +0300

    Nodarbiba_05

--- Nodarbiba_05/JS/script.js
+++ Nodarbiba_05/JS/script.js
@@ -2,6 +2,10 @@
 
 let now = new Date();
 let hour_x = 20;
+let morning_hour = 10;
+let day_hour = 18;
+let night_hour_a = 22;
+let night_hour_b = 4;
 
 console.log(now);
 console.log(typeof(now));
@@ -24,8 +28,26 @@ if (now.getHours() < hour_x) {
      document.getElementById("demo").innerHTML = "Good day!";
 }
 */
+/*
 if (0.1) {
     console.log ("Output Nr.1 from if.");
     console.log ("Output Nr.2 from if.");
      document.getElementById("demo").innerHTML = "Good day!";
+}
+*/
+
+if (now.getHours() > night_hour_b && now.getHours() < morning_hour) {
+     document.getElementById("demo").innerHTML = "Good morning!";
+}
+
+if (now.getHours() > morning_hour && now.getHours() < day_hour) {
+    document.getElementById("demo").innerHTML = "Good day!";
+}
+
+if (now.getHours() > day_hour && now.getHours() < night_hour_a) {
+    document.getElementById("demo").innerHTML = "Good evening!";
+}
+
+if (now.getHours() > night_hour_a && now.getHours() < night_hour_b) {
+    document.getElementById("demo").innerHTML = "Good night!";
 }
