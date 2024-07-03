3578e0ec4e65a3109788d13940fe1eea58773bed
EgitaZ <egita.s@inbox.lv>
2024-07-03 11:14:43 +0300

    Nodarbiba_05

--- Nodarbiba_05/JS/script.js
+++ Nodarbiba_05/JS/script.js
@@ -1,9 +1,31 @@
 // if (nosacjums){darbIbas;}
 
-console.log(new Date());
-console.log(new Date().getHours());
-console.log(new Date().getHours() < 20);
+let now = new Date();
+let hour_x = 20;
 
-if (new Date().getHours() < 20) {
+console.log(now);
+console.log(typeof(now));
+
+console.log(now.getHours());
+console.log(typeof(now.getHours()));
+
+console.log(now.getHours() < hour_x);
+console.log(typeof(now.getHours() < hour_x));
+/*
+if (now.getHours() < hour_x) {
+   console.log ("Output from if.");
     document.getElementById("demo").innerHTML = "Good day!";
+}
+*/
+/*
+if (now.getHours() < hour_x) {
+    console.log ("Output Nr.1 from if.");
+    console.log ("Output Nr.2 from if.");
+     document.getElementById("demo").innerHTML = "Good day!";
+}
+*/
+if (0.1) {
+    console.log ("Output Nr.1 from if.");
+    console.log ("Output Nr.2 from if.");
+     document.getElementById("demo").innerHTML = "Good day!";
 }
