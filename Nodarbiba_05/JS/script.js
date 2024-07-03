e7e2348d8dec790156618673a5a2275b918f5c63
EgitaZ <egita.s@inbox.lv>
2024-07-03 10:24:59 +0300

    Nodarbiba_05

--- Nodarbiba_05/JS/script.js
+++ Nodarbiba_05/JS/script.js
@@ -1,3 +1,9 @@
-if (new Date().getHours() < 18) {
+// if (nosacjums){darbIbas;}
+
+console.log(new Date());
+console.log(new Date().getHours());
+console.log(new Date().getHours() < 20);
+
+if (new Date().getHours() < 20) {
     document.getElementById("demo").innerHTML = "Good day!";
 }
